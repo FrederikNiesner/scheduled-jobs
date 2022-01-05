c3637b4d3dfa6c2025c43ce62ff3b649d1862a72
FrederikNiesner <frederik.niesner@gmail.com>
2022-01-05 18:17:09 +0100

    zsh path fix

--- internetSpeed/schedule.sh
+++
@@ -1,9 +0,0 @@
-export PATH=/usr/local/bin:$PATH
-cd Google\ Drive/Code/Repositories/scheduled-jobs/internetSpeed
-git pull
-python3 internetSpeed.py
-git add .
-git commit -m "scheduled speedtest `date`"
-git push
-
-
--- internetSpeed/schedule.zsh
+++ internetSpeed/schedule.zsh
@@ -1,7 +1,9 @@
-#!/bin/sh
+export PATH=/usr/local/bin:$PATH
 cd Google\ Drive/Code/Repositories/scheduled-jobs/internetSpeed
 git pull
 python3 internetSpeed.py
 git add .
 git commit -m "scheduled speedtest `date`"
 git push
+
+
