f0128ba0a08e3ea88d30690cbdcfd5c2edc1d86e
FrederikNiesner <frederik.niesner@gmail.com>
2022-01-05 18:16:04 +0100

    scheduled speedtest Wed Jan  5 18:16:04 CET 2022

--- internetSpeed/schedule.zsh
+++ internetSpeed/schedule.zsh
@@ -1,9 +1,7 @@
-export PATH=/usr/local/bin:$PATH
+#!/bin/sh
 cd Google\ Drive/Code/Repositories/scheduled-jobs/internetSpeed
 git pull
 python3 internetSpeed.py
 git add .
 git commit -m "scheduled speedtest `date`"
 git push
-
-
